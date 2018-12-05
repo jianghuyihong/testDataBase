612200f71d12d42f385f500ea07d97d093db3ce0
chenji <chenji@sunkongedu.com>
2018-12-05 10:36:17 +0800

    v1.0000

---
+++ testDataBase/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace testDataBase
+{
+    public class BundleConfig
+    {
+        // 有关捆绑的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Content/javaScript/layui.js",
+                        "~/Content/javaScript/layui.all.js"
+                        ));
+            
+        
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/stylesheet/layui.css",
+                      "~/Content/stylesheet/StyleSheet.css",
+                      "~/Content/stylesheet/layui.mobile.css"
+                      ));
+        }
+    }
+}
---
+++ testDataBase/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 测试数据库连接.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
